6eea32c9e7e404f07e9da57ee6991fbfc41322e4
Tarek Yahia <thebottarek@gmail.com>
2025-07-16 05:10:09 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import { FaLinkedinIn } from 'react-icons/fa'
 import { MdEmail } from "react-icons/md";
 import { Link } from 'react-router-dom'
 
+
 function Footer() {
   const year = new Date().getFullYear()
   return (
